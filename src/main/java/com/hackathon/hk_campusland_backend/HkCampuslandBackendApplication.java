aee88764f63a68ba8ddd3503d532dad493a658d9
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-09-18 20:38:59 -0500

    first commit

---
+++ src/main/java/com/hackathon/hk_campusland_backend/HkCampuslandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.hk_campusland_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkCampuslandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkCampuslandBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/hk_campusland_backend/HkCampuslandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.hk_campusland_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkCampuslandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
