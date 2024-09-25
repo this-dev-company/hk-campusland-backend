30a871e71e0647c17691faf2da9bdcdd0253f852
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-09-25 13:27:53 -0500

    feat: add updated and delete to tasks endpoints

--- src/main/java/com/hackathon/hk_campusland_backend/tareas/application/TareaServiceImpl.java
+++ src/main/java/com/hackathon/hk_campusland_backend/tareas/application/TareaServiceImpl.java
@@ -12,6 +12,8 @@ import com.hackathon.hk_campusland_backend.tareas.domain.services.TareaService;
 import com.hackathon.hk_campusland_backend.tareas.infrastructure.repositories.TareaRepository;
 import com.hackathon.hk_campusland_backend.utils.exception.dto.BusinessException;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,6 +24,7 @@ public class TareaServiceImpl implements TareaService {
     private TareaRepository tareaRepository;
 
     @Override
+    @Transactional
     public Optional<Tarea> findById(Long id) {
 
         if (!tareaRepository.findById(id).isPresent()) {
@@ -32,6 +35,7 @@ public class TareaServiceImpl implements TareaService {
     }
 
     @Override
+    @Transactional
     public List<Tarea> getAll() {
         List<Tarea> Tareaes = tareaRepository.findAll();
 
@@ -43,7 +47,37 @@ public class TareaServiceImpl implements TareaService {
     }
 
     @Override
+    @Transactional
     public Tarea save(Tarea Tarea) {
         return tareaRepository.save(Tarea);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Tarea tarea) {
+        tareaRepository.delete(tarea);
+    }
+
+    @Override
+    @Transactional
+    public void update(Long id, Tarea tarea) {
+        Optional<Tarea> existingTareaOpt = tareaRepository.findById(id);
+
+        if (existingTareaOpt.isPresent()) {
+            Tarea existingTarea = existingTareaOpt.get();
+
+            existingTarea.setNombre(tarea.getNombre());
+            existingTarea.setDescripcion(tarea.getDescripcion());
+            existingTarea.setStartAt(tarea.getStartAt());
+            existingTarea.setEndAt(tarea.getEndAt());
+            existingTarea.setHorasEstimadas(tarea.getHorasEstimadas());
+            existingTarea.setProyecto(tarea.getProyecto());
+            existingTarea.setTipoTarea(tarea.getTipoTarea());
+            existingTarea.setEstadoTarea(tarea.getEstadoTarea());
+
+            tareaRepository.save(existingTarea);
+        } else {
+            throw new EntityNotFoundException("Tarea not found with id: " + id);
+        }
     }
 }
--- src/main/java/com/hackathon/hk_campusland_backend/tareas/infrastructure/controllers/TareaController.java
+++ src/main/java/com/hackathon/hk_campusland_backend/tareas/infrastructure/controllers/TareaController.java
@@ -1,13 +1,17 @@
 package com.hackathon.hk_campusland_backend.tareas.infrastructure.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,13 +34,13 @@ public class TareaController {
     public ResponseEntity<List<Tarea>> listTareaes() {
         List<Tarea> tareas = tareaService.getAll();
         return new ResponseEntity<>(tareas, HttpStatus.OK);
-    } 
+    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Tarea> showTarea(@PathVariable Long id){
+    public ResponseEntity<Tarea> showTarea(@PathVariable Long id) {
         return tareaService.findById(id)
-            .map(tarea -> new ResponseEntity<>(tarea, HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .map(tarea -> new ResponseEntity<>(tarea, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
@@ -44,4 +48,38 @@ public class TareaController {
         Tarea newTarea = tareaService.save(tarea);
         return new ResponseEntity<>(newTarea, HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteTarea(@PathVariable Long id) {
+        try {
+            Optional<Tarea> tarea = tareaService.getAll().stream().filter(s -> s.getId().equals(id)).findFirst();
+            if (tarea.isPresent()) {
+                tareaService.delete(tarea.get());
+                return ResponseEntity.ok("Tarea deleted successfully");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarea not found with id: " + id);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error deleting Tarea: " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateTarea(@PathVariable Long id, @Valid @RequestBody Tarea updatedTarea,
+            BindingResult result) {
+        try {
+            Optional<Tarea> existingTareaOptional = tareaService.findById(id);
+            if (existingTareaOptional.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarea not found with id: " + id);
+            }
+
+            tareaService.update(id, updatedTarea);
+            return ResponseEntity.ok("Tarea updated successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error updating Tarea: " + e.getMessage());
+        }
+    }
+
 }
